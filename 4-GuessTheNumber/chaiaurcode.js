bd9e4982a0c702f5fbfe788a7691f905c3a4f56b
paglaghoda <omkarsahay5@gmail.com>
2025-08-15 01:40:46 +0530

    new-way for project 4

--- 4-GuessTheNumber/chaiaurcode.js
+++ 4-GuessTheNumber/chaiaurcode.js
@@ -1,51 +1,99 @@
 //Declaring
-const canPlay=true;
-
-const guess = parseInt(document.querySelector('#guessField').value)
+let canHePlay=true;
 const submit = document.querySelector('#subt')
+const target = randomNum()
 
-const prevGuess = [];
-
+let prevGuess = [];
+let noOfGuessesRem = 10;
 const guesses = document.querySelector('.guesses')
 const lastResult = document.querySelector('.lastResult')
 const lowOrHigh = document.querySelector('.lowOrHi')
+const resultDiv = document.querySelector('.resultParas')
 
 //Functions
 
 function randomNum(){
-  return Math.floor(Math.random()*(100-1))+1
+  let ranNum = Math.floor(Math.random()*(100))+1
+  return ranNum
 }
 
 function validateValue(guess){
-  if (guess<1 || guess>100 || isNaN(guess)) {
-    displayMessage()
+  if (guess<1) {
+    displayErrorMessage(`Enter a valid number greater than 1`)
+    return false
+  } else if (guess>100) {
+    displayErrorMessage(`Enter a valid number less than 100`)
+    return false
+  } else if (isNaN(guess)) {
+    displayErrorMessage(`Enter a valid integer`)
     return false
   } else {
     return true
   }
 }
 
-function checkGuess(guess){
-  const randomNum = randomNum()
-  if (validateValue(guess)) {
-    if (guess === randomNum) {
-      displayGuess()
-    } else if (guess>randomNum) {
-      //Display Msg too High
-    } else {
-      //Display Msg too Low
-    }
+function canPlay(){
+  if (noOfGuessesRem<1 && canHePlay){
+    const p = document.createElement('p')
+    p.style.backgroundColor="red"
+    p.style.color="white"
+    p.style.padding="15px";
+    p.style.textAlign="center"
+    p.appendChild(document.createTextNode("Guess Limit Khatam! GAME OVER LIL' BRO!"))
+    resultDiv.appendChild(p)
+    canHePlay=false;
+    submit.disabled=true;
+    return false
   } else {
-    displayErrorMessage()
+    return true
   }
 }
 
-function displayGuess(){
+function checkGuess(guess, randomNumber){
+  if (validateValue(guess)) {
 
+    noOfGuessesRem--
+    prevGuess.push(guess)
+    displayPrevGuess()
+    displayRemGuess()
+    canPlay()
+
+    if (guess == randomNumber) {
+      displayCorrectGuess("Ek Crore! (in Big B's voice)")
+      canHePlay=false;
+      submit.disabled=true;
+    } else if (guess>randomNumber) {
+      displayErrorMessage("Guess is too High")
+    } else {
+      displayErrorMessage("Guess is too Low")
+    }
+  }
 }
 
-function displayErrorMessage(){
+function displayCorrectGuess(success){
+  const p = document.createElement('p')
+  p.style.backgroundColor="green"
+  p.style.color="white"
+  p.style.padding="15px";
+  p.style.textAlign="center"
+  p.appendChild(document.createTextNode(success))
+  resultDiv.appendChild(p)
 }
 
-checkGuess(guess)
+function displayPrevGuess(){
+  guesses.innerHTML=prevGuess
+}
 
+function displayErrorMessage(error){
+  lowOrHigh.innerText=error;
+}
+
+function displayRemGuess(){
+  lastResult.innerHTML=noOfGuessesRem
+}
+
+submit.addEventListener('click', (e) => {
+  e.preventDefault()
+  const guess = parseInt(document.querySelector('#guessField').value)
+  checkGuess(guess, target)
+})
